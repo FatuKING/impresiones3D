89bb0a5bf65cb1d167993832f6c9fb40254ea389
Facundo Cozzani <cozzanifacundo@gmail.com>
2023-12-04 14:59:48 -0300

    Tarjetas de productos terminadas

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import './style.css'
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  AOS.init({
+    offset: 100, 
+    duration: 800, 
+    easing: 'ease-in-out', 
+    delay: 200, 
+    once: true, 
+  });
+});
